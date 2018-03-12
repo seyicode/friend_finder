a7aa4421897ccaf0a18184b882864e069b818f40
Seyi <seyiekundayo879@gmail.com>
2018-03-11 23:05:46 -0500

    updated working codes

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Donald Trump',
+		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Muhamadu buhari',
+		photo: 'https://cdn.cnn.com/cnnnext/dam/assets/170127133639-01-muhammadu-buhari-file-exlarge-169.jpg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Robert Mugabe',
+		photo: 'http://thesoutherndaily.co.za/wp-content/uploads/2016/08/mugabe.jpeg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Jacob Zuma',
+		photo: 'http://metrowatchonline.com/wp-content/uploads/2018/02/img_2409-1.jpeg',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+//=============================================================
+// Dependencies
+// ============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+
+//=============================================================
+// Sets up the Express App
+//=============================================================
+
 var app = express();
+var PORT = 3000;
 
-var PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-//require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes.js")(app);
+
+//=============================================================
+//Routing
+//=============================================================
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
 
+// =============================================================
+// Starts the server to begin listening 
+// =============================================================
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT ' + PORT);
+});
